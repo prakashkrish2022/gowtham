9b9d10802eae1d5e7912f12f474375cd38516707
prakashkrish2022 <prakashmech925@gmail.com>
2022-04-22 17:27:32 -0700

    new globe code

---
+++ src/fileoperation/Files.java
@@ -0,0 +1,43 @@
+package fileoperation;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Files {
+
+	public static void main(String[] args) throws IOException {
+
+		File f= new File("E:\\prakash");
+		
+		
+		boolean mkdir = f.mkdir();
+		System.out.println(mkdir);
+		
+		boolean mkdirs = f.mkdirs();
+		System.out.println(mkdirs);
+		
+		File f1= new File("E:\\prakash\\aa\\a2");
+		
+		boolean createNewFile = f1.createNewFile();
+		System.out.println(createNewFile);
+		
+		boolean directory = f.isDirectory();
+		System.out.println(directory);
+		
+		
+		boolean file = f1.isFile();
+		System.out.println(file);
+		
+		boolean canRead = f1.canRead();
+		System.out.println(canRead);
+		
+		boolean canRead2 = f1.canRead();
+		System.out.println(canRead2);
+		
+		boolean canExecute = f1.canExecute();
+		System.out.println(canExecute);
+		
+
+	}
+
+}
